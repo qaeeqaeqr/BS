0fcde7abb84cb1cc71739a4bd4e695036edb38a5
王钰-HP-Large <2796672023@qq.com>
2024-12-12 16:26:16 +0800

    20241212-envs

--- README.md
+++ README.md
@@ -6,10 +6,37 @@
 
 本项目主要将CTD对于回报的均值和方差的预测扩展到了多智能体的情况。
 
+### Environment
+
+##### 1 Code environment
+本项目在windows下运行，python=3.9，
+依赖库见[requirements.txt](requirements.txt)。
+
+注意，在windows下，由于multi-agent-ale-py包的限制，
+pettingzoo的atari环境均不可用。
+
+##### 2 MARL environment
+
+计划使用pettingzoo的以下环境：
+
+* from pettingzoo.sisl import pursuit_v4
+* from pettingzoo.butterfly import cooperative_pong_v5
+* from pettingzoo.classic import connect_four_v3
+
+这些环境详见pettingzoo官方文档[pettingzoo](https://pettingzoo.farama.org/)
+
 ### Project
 
 [assets](./assets)中包含多智能体均值-方差线性组合最小算法的公式推导的tex文档。
 
 [docs](./docs)中包含毕业设计的过程文档。
+
+[configs](./configs)包含算法参数的配置。
+
+[envs.py](./envs.py)中包含MARL的环境及其配置。
+
+[test.py](./tests.py)中是一些测试代码，可忽略。
+
+
 
 
--- tests.py
+++ tests.py
@@ -1,28 +1,24 @@
 import pettingzoo
-from pettingzoo.classic import tictactoe_v3
 import random
 import numpy as np
+from envs import persuit_env, pong_env, connect4_env
 
 def pettingzoo_env_test():
-    # 创建井字棋环境
-    env = tictactoe_v3.env(render_mode="human")
-    # 重置环境以开始新的游戏
+    env = persuit_env()
     env.reset(seed=42)
-    # 循环直到游戏结束
+
     for agent in env.agent_iter():
         observation, reward, termination, truncation, info = env.last()
 
         if termination or truncation:
             action = None
         else:
-            mask = observation["action_mask"]
-            # 随机选择一个可用的动作
-            action = np.random.choice(np.where(mask)[0])
+            # this is where you would insert your policy
+            action = env.action_space(agent).sample()
 
         env.step(action)
+    env.close()
 
-    # 渲染最终的环境状态
-    env.render()
 
 if __name__ == "__main__":
-    pass
+    pettingzoo_env_test()
